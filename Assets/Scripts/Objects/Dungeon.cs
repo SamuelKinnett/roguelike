0b5d48100df13d2c63d7182a31810a0c3b8d4dee
= <samuelkinnett96@googlemail.com>
2016-04-18 15:41:25 +0100

    Began implementing further dungeon improvements

--- Assets/Scripts/Objects/Dungeon.cs
+++ Assets/Scripts/Objects/Dungeon.cs
@@ -10,11 +10,19 @@ public class Dungeon
 {
 
 	int numberOfFloors;
+	int[] enemyLevelRange;
+	string dungeonName;
+	string mapPalette;
+
+	FileManager fileManager;
 	List<DungeonFloor> floors;
 
 	public Dungeon ()
 	{
 		floors = new List<DungeonFloor> ();
+		fileManager = GameObject.Find ("FileManager").GetComponent<FileManager>();
+
+		GenerateName ();
 	}
 
 	public void AddFloor (int width, int height, int[,] collisionMap, int[] stairLocations, bool bottomFloor)
@@ -31,6 +39,83 @@ public class Dungeon
 	public void SetVisibilityMap (TileVisibility[,] visibilityMap, int level)
 	{
 		floors [level].SetVisibilityMap (visibilityMap);
+	}
+
+	void GenerateName() {
+		int roll = (int)Mathf.Round (5 * Random.value);
+		string owner;
+
+		switch (roll) {
+
+		case 0:
+			//prefix and first
+			//e.g. "The whispering meadow"
+
+			dungeonName = "The " + fileManager.GetDungeonPrefix() + " " + fileManager.GetDungeonFirstName() + ".";
+
+			break;
+
+		case 1:
+			//first and suffix
+			//e.g. "The caves of sorrow"
+
+			dungeonName = "The " + fileManager.GetDungeonFirstName () + " of " + fileManager.GetDungeonSuffix() + ".";
+
+			break;
+
+		case 2:
+			//first and second
+			//e.g. "The Castle of Jim"
+
+			dungeonName = "The " + fileManager.GetDungeonFirstName () + " of " + fileManager.GetDungeonSecondName() + ".";
+
+			break;
+
+		case 3:
+			//second and first
+			//e.g. "Sen's fortress"
+
+			owner = fileManager.GetDungeonSecondName ();
+			if (owner.ToCharArray () [owner.ToCharArray ().Length - 1] == 's')
+				owner += "'";
+			else
+				owner += "'s";
+
+			dungeonName = owner + " " + fileManager.GetDungeonFirstName () + ".";
+
+			break;
+
+		case 4:
+			//second, first and suffix
+			//e.g. "Sen's fortress of clanging"
+
+			owner = fileManager.GetDungeonSecondName ();
+			if (owner.ToCharArray () [owner.ToCharArray ().Length - 1] == 's')
+				owner += "'";
+			else
+				owner += "'s";
+
+			dungeonName = owner + " " + fileManager.GetDungeonFirstName () + " of " + fileManager.GetDungeonSuffix() + ".";
+
+			break;
+
+		case 5:
+			//second, prefix, first and suffix
+			//e.g. "Sen's massive fortress of smashing"
+
+			owner = fileManager.GetDungeonSecondName ();
+			if (owner.ToCharArray () [owner.ToCharArray ().Length - 1] == 's')
+				owner += "'";
+			else
+				owner += "'s";
+
+			dungeonName = owner + " " + fileManager.GetDungeonPrefix() + " "
+				+ fileManager.GetDungeonFirstName () + " of " + fileManager.GetDungeonSuffix() + ".";
+			break;
+
+		}
+
+		Debug.Log (dungeonName);
 	}
 
 }
